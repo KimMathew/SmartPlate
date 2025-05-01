3a1bb2745a35bad71dbeec6e9c097e455d757473
gianezekielsgersaniba@gmail.com <gianezekielsgersaniba@gmail.com>
2025-05-01 17:18:28 +0800

    fix the padding layout

--- components/dashboard-layout.tsx
+++ components/dashboard-layout.tsx
@@ -18,7 +18,11 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
             <SidebarTrigger />
           </div>
         </div>
-        <div className="flex-1 px-6 py-6 w-full">{children}</div>
+        <div className="flex-1 w-full px-4 sm:px-6 lg:px-8 py-6">
+          <div className="container mx-auto">
+            {children}
+          </div>
+        </div>
       </main>
     </SidebarProvider>
   );
